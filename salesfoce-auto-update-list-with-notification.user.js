b883e66d96c69608f1ae12b6874bd62069d3db27
Sven Heidemann <sven.heidemann@sh-edraft.de>
2022-09-23 12:40:01 +0200

    Improved check for new tickets

--- salesfoce-auto-update-list-with-notification.user.js
+++ salesfoce-auto-update-list-with-notification.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Salesforce List Auto Update with notification
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @updateURL    https://github.com/123erfasst/tampermonkey-scripts/raw/master/salesfoce-auto-update-list-with-notification.user.js
 // @downloadURL  https://github.com/123erfasst/tampermonkey-scripts/raw/master/salesfoce-auto-update-list-with-notification.user.js
 // @description  Automatically update Salesforce lists so tickets are up to date
 // @author       Sven Heidemann, Benno Pohl
 // @grant        GM_notification
+// @grant        GM_log
 // @grant        GM.setValue
 // @grant        GM.getValue
 // @match        https://nevaris.lightning.force.com/lightning/o/Case/list*
@@ -14,34 +15,72 @@
 
 
 'use strict';
-function getTicketCount() {
-    var select = document.querySelector('#brandBand_1 > div > div > div > div > div.slds-grid.listDisplays.safari-workaround-anchor > div > div.slds-col.slds-no-space.forceListViewManagerPrimaryDisplayManager > div.undefined.forceListViewManagerGrid > div.listViewContent.slds-table--header-fixed_container > div.uiScroller.scroller-wrapper.scroll-bidirectional.native > div > div > table > tbody');
-    return select.querySelectorAll('tr').length
+function getTicketList() {
+    return document.querySelector('#brandBand_1 > div > div > div > div > div.slds-grid.listDisplays.safari-workaround-anchor > div > div.slds-col.slds-no-space.forceListViewManagerPrimaryDisplayManager > div.undefined.forceListViewManagerGrid > div.listViewContent.slds-table--header-fixed_container > div.uiScroller.scroller-wrapper.scroll-bidirectional.native > div > div > table > tbody')
 }
 
-var reloadDelaySeconds = await GM.getValue('ReloadDelaySeconds', 30)
+function getTicketCount() {
+    return getTicketList().querySelectorAll('tr').length
+}
 
-setTimeout(async () => {
+function getTicketIds() {
+    var ids = [];
+    var ticketListLinks = getTicketList().querySelectorAll('tr > td:nth-child(4) > span > a');
+    ticketListLinks.forEach(element => {
+        ids.push(element.innerText || element.textContent);
+    });
+    return ids;
+}
+
+function compareTickets(oldTickets, newTickets) {
+    var closedTickets = [];
+    var openedTickets = [];
+    newTickets.forEach(newID => {
+        if (!oldTickets.includes(newID)) {
+            openedTickets.push(newID);
+        }
+    });
+    oldTickets.forEach(newID => {
+        if (!newTickets.includes(newID)) {
+            closedTickets.push(newID);
+        }
+    });
+    return {
+        closed: closedTickets,
+        opened: openedTickets
+    };
+}
+
+function notify(closed, opened) {
+    if (closed.length > 0) {
+        GM_notification({ title: 'SF geschlossene Tickets', text: `Geschlossene Tickets (${closed.length}): ${closed.toString()}`});
+    }
+
+    if (opened.length > 0) {
+        GM_notification({ title: 'SF geöffnete Tickets', text: `Geöffnete Tickets (${opened.length}): ${opened.toString()}`});
+    }
+}
+
+async function validateTicketCount() {
     if (!window.location.href.includes("https://nevaris.lightning.force.com/lightning/o/Case/list")) {
         return;
     }
-    var newTicketCount = getTicketCount();
-    var oldTicketCount = await GM.getValue('TicketCount');
-    await GM.setValue('TicketCount', newTicketCount);
-    if (newTicketCount == oldTicketCount) {
-        return;
-    }
+    GM_log('tampermonkey: Compare tickets');
+    var oldTickets = await GM.getValue('Tickets');
+    var newTickets = getTicketIds();
+    await GM.setValue('Tickets', newTickets);
+    var result = compareTickets(oldTickets, newTickets);
+    notify(result.closed, result.opened);
+}
 
-    if (newTicketCount > oldTicketCount) {
-        GM_notification({ title: 'SF neue Tickets', text: `Es sind ${newTicketCount - oldTicketCount} neue Tickets vorhanden`});
-    } else if (newTicketCount < oldTicketCount) {
-        GM_notification({ title: 'SF weniger Tickets', text: `Es sind ${oldTicketCount - newTicketCount} weniger Tickets vorhanden`});
-    }
-}, 10 * 1000);
+setTimeout(async () => {
+    await validateTicketCount();
+}, await GM.getValue('CompareTicketsDelaySeconds', 10) * 1000);
 
 setTimeout(() => {
     if (!window.location.href.includes("https://nevaris.lightning.force.com/lightning/o/Case/list")) {
         return;
     }
+    GM_log('tampermonkey: reload');
     location.reload();
-}, reloadDelaySeconds * 1000);
+}, await GM.getValue('ReloadDelaySeconds', 30) * 1000);
